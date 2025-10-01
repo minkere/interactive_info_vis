54089595a23a83044c8f207686116269849dffd1
minkere <minkere715@gmail.com>
2025-10-01 13:24:56 -0700

    Changed shape of sun and color of the sky and grass depending on mouse position.

--- sketch.js
+++ sketch.js
@@ -14,34 +14,49 @@ function draw() {
   //shape follows y-coordinate of mouse
   shapeHeight = mouseY;
   currentWidth = mouseX;
+  inverseShapeHeight = -mouseY
 
   //light blue background if the shape is above horizon
 
   //with if-else statement
   if (shapeHeight < horizon) {
-    background("lightblue"); // blue if above horizon
+    background("#ddf0f0"); // Sky blue if above horizon
     
   } else {
-    background("grey"); // grey if below horizon
+    background("#3b3b45"); // Dark greyish-blue if below horizon
   }
 
   //sun
-  fill("white");
+  fill("#fcdf6a"); // Sun color
+  circle(width/8, shapeHeight, width/4);
+
+//moon
+  fill("white"); // Moon color
+  circle(width/8, inverseShapeHeight+800, width/4);
   
-  rect(width/4, shapeHeight, width/2);
   textSize(20);
-  fill("black");
+  if (shapeHeight < horizon) {
+    fill("black"); // Text is black if above horizon
+    
+  } else {
+    fill("white"); // Text is white if below horizon
+  }
   text('Hello! My name is Minkyu Kim!', currentWidth/2, shapeHeight/2);
   
 
 
   // draw line for horizon
-  stroke('lavender');
+  stroke('black');
   line(0,horizon,width,horizon);
 
   //grass
 
-  fill("lavender");
+  if (shapeHeight < horizon) {
+    fill("#73b370"); // Grass is light green if above horizon
+    
+  } else {
+    fill("#314a30"); // Grass is dark green if below horizon
+  }
 
   rect(0, horizon, width, height);
 
