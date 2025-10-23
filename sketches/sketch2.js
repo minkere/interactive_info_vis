6cc470af22609d3200c9b934aa3e24c5f7dcc38a
minkere <minkere715@gmail.com>
2025-10-23 16:10:52 -0700

    Cleaned up borders between clock sections and timer hand.

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -21,30 +21,54 @@ registerSketch('sk2', function (p) {
     p.translate(p.width / 2, p.height / 2);
     const R = p.min(p.width, p.height) * 0.45;
 
-    // Drawing the Clock Face
+    // Egg White Clock Base
+    p.strokeWeight(1.5);
     p.fill(245, 245, 245);
     p.ellipse(0, 0, 550, 550);
-
-    p.fill("darkgrey");
+    p.fill(230, 230, 230);
     p.arc(0, 0, 550, 550, -90, -30);
-    p.fill("lightgrey");
+    p.fill(235, 235, 235);
     p.arc(0, 0, 550, 550, -30, 30);
-    p.fill("white");
+    p.fill(240, 240, 240);
     p.arc(0, 0, 550, 550, 30, 90);
+
+    // Egg Yolk Clock Base
+    p.stroke(255, 199, 0);
     p.fill(255, 235, 125);
     p.ellipse(0, 0, 200, 200);
+    p.fill(255, 209, 25);
+    p.arc(0, 0, 200, 200, -90, -30);
+    p.fill(255, 225, 94);
+    p.arc(0, 0, 200, 200, -30, 30);
+    p.fill(255, 230, 140);
+    p.arc(0, 0, 200, 200, 30, 90);
     
     // Drawing the Clock Face Boundaries
-    p.stroke("black");
-    p.strokeWeight(1);
+    p.stroke("grey");
+    p.strokeWeight(1.5);
     p.line(0, -275, 0, -100);
-    p.line(0, +275, 0, 100);
+    p.line(0, 275, 0, 100);
+
+    p.stroke(255, 199, 0);
+    p.line(0, -100, 0, 100);
+
     p.push();
+    p.stroke("grey");
     p.rotate(60);
     p.line(0, -100, 0, -275);
+    p.stroke(255, 199, 0);
+    p.line(0, 0, 0, -100);
+    
+    p.stroke("grey");
     p.rotate(60)
     p.line(0, -100, 0, -275);
+    p.stroke(255, 199, 0);
+    p.line(0, 0, 0, -100);
     p.pop();
+
+    p.stroke("black");
+
+    // Adding Boiled Egg Doneness Text
 
     // Calculating Total Seconds Left in the 10-Minute Countdown
     let totalsecs = Math.floor(600 - (mils / 1000));
@@ -57,13 +81,14 @@ registerSketch('sk2', function (p) {
     // Timer Hand
     p.push();
     p.rotate(totalsecAngle);
+    p.stroke(180, 50, 50)
     p.strokeWeight(2);
-    p.line(0, 0, 0, -R * 0.7);
+    p.line(0, -101, 0, -R * 0.76);
     p.pop();
 
     p.textAlign(p.CENTER);
     p.textSize(30);
-    p.text(totalsecAngle + " " + mins + " : " + secs , 0, 0 + 350);
+    p.text(mins + " : " + secs , 0, 0 + 350);
   }
   
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
