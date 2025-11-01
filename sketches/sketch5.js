e5f260d37ed8d52528f5f430699ba6a1d7390ff9
minkere <minkere715@gmail.com>
2025-10-31 20:27:06 -0700

    Added data file to repo

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,24 +1,21 @@
 // Example 2
 registerSketch('sk5', function (p) {
+  let data;
+
+  p.preload = function () {
+    // Load CSV file from data folder
+    data = p.loadTable('data/FoodImports.csv', 'csv', 'header');
+  }
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(1080, 1350);
+
+    console.log(data.getRowCount() + ' total rows in table');
+    console.log(data.getColumnCount() + ' total columns in table');
   };
 
   p.draw = function () {
-    p.background(250);
-
-    // Corner time display
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
-
-    p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
-    p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
-
+    p.background('lightgrey');
   }
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
