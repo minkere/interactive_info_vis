03e1eb922aa64acd28c2ed03ffdcd83153664ace
minkere <minkere715@gmail.com>
2025-11-01 22:59:32 -0700

    Cleaned up sketch5.js to prepare for embedding Tableau Vis

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,22 +1,14 @@
 // Example 2
 registerSketch('sk5', function (p) {
-  let data;
-
-  p.preload = function () {
-    // Load CSV file from data folder
-    data = p.loadTable('data/FoodImports.csv', 'csv', 'header');
-  }
-
   p.setup = function () {
     p.createCanvas(1080, 1350);
-
-    console.log(data.getRowCount() + ' total rows in table');
-    console.log(data.getColumnCount() + ' total columns in table');
   };
 
   p.draw = function () {
-    p.background('lightgrey');
+    p.background("lightgrey");
   }
 
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 });
