46318c1aaa207a34e2e175351d83b9f7c67da218
minkere <minkere715@gmail.com>
2025-11-01 23:12:13 -0700

    Added Tableau dashboard and further cleaned sketch5.js

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,14 +1,14 @@
 // Example 2
 registerSketch('sk5', function (p) {
   p.setup = function () {
-    p.createCanvas(1080, 1350);
+    //
   };
 
   p.draw = function () {
-    p.background("lightgrey");
+    //
   }
 
   p.windowResized = function () {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    //
   };
 });
