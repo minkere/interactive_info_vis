f9921f4365fa8a3c4054bd30a3f0e58a56d097a1
minkere <minkere715@gmail.com>
2025-11-01 23:16:20 -0700

    Redid some of the functions in sketch5.js to ensure the Tableau dashboard properly appears

--- sketches/sketch5.js
+++ sketches/sketch5.js
@@ -1,14 +1,14 @@
 // Example 2
 registerSketch('sk5', function (p) {
   p.setup = function () {
-    //
+    p.createCanvas(650, 5);
   };
 
   p.draw = function () {
-    //
+    p.background("lightgrey");
   }
 
   p.windowResized = function () {
-    //
+    p.resizeCanvas(650, 860);
   };
 });
